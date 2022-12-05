dc2a5af5a460007c5e0bddecbbee7db785face5f
Vitor <vitorhugomota99@hotmail.com>
2022-12-05 01:24:52 -0300

    Update Teste.java

--- Relogio/src/Teste.java
+++ Relogio/src/Teste.java
@@ -2,11 +2,14 @@
 public class Teste {
 
 	public static void main(String[] args) {
+		//Creating my new object from Relogio class
 		Relogio relogio = new Relogio();
 		
 		for(int x = 0; x <= 23; x++){
 			for(int y = 0; y <= 59; y++) {
+				//Making my clock work with TIC TAC method
 				relogio.TicTac();
+				//Show
 				System.out.println(relogio.exibe());
 			}
 		}
